fb814751423a740154b6fd4af41d8d0051ea3900
dev-EliMz <elielmunizs3@gmail.com>
2026-03-31 11:04:25 -0300

    findByDepartment adicionado ao sellerDaoJdbc

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,10 +10,18 @@ import model.entities.Seller;
 public class Programm {
 	public static void main(String[] args) {
 		
+		System.out.println("--- TEST 1: findById on seller ---");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("\n--- TEST 2: findByDepartment on seller ---");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for (Seller seller2 : list) {
+			System.out.println(seller2);
+		}
+		
 	}
 }
