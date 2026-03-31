d0c6af1097d90257113d571d549dcb478cd5c111
dev-EliMz <elielmunizs3@gmail.com>
2026-03-31 09:41:58 -0300

    Department entity class adicionado

---
+++ src/application/Programm.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Programm {
+	public static void main(String[] args) {
+		Department obj = new Department(8, "Books");
+		System.out.println(obj);
+	}
+}
